35e44718aa3d0e565691e7b461c1df76971bf84d
Anjacodes <anja_schmidt7@posteo.de>
2022-05-03 13:15:23 -0500

    Connect React App to Rails API

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Greeting from './Greeting'
+import Greeting from './components/Greeting'
 
 import './App.css';
+import { Provider } from 'react-redux';
+import store from './redux/configureStore';
 
 const App = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={< Greeting />} />
-    </Routes>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={< Greeting />} />
+      </Routes>
+    </BrowserRouter>
+  </Provider>
 )
 
 export default App;
---
+++ src/components/Greeting.js
@@ -0,0 +1,24 @@
+import React, {useEffect} from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchGreetings } from "../redux/greeting";
+
+const Greeting = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchGreetings());
+  }, [dispatch]);
+
+  const message = useSelector((state) => state.greetingsReducer)
+
+  console.log(message);
+
+  return (
+    <div>
+      <h1>Greeting</h1>
+      <p>Greeting: {message.message}</p>
+    </div>
+  )
+}
+
+export default Greeting;
